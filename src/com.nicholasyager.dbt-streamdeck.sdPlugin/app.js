eaedc307b30bc3bfa26e25560e21675d77e3e43d
Nicholas Yager <yager@nicholasyager.com>
2024-02-09 14:52:17 -0500

    feat: Add job monitoring

--- src/com.nicholasyager.dbt-streamdeck.sdPlugin/app.js
+++ src/com.nicholasyager.dbt-streamdeck.sdPlugin/app.js
@@ -4,6 +4,7 @@
 const myAction = new Action("com.nicholasyager.dbt-streamdeck.action");
 
 const model = new Action("com.nicholasyager.dbt-streamdeck.model");
+const job = new Action("com.nicholasyager.dbt-streamdeck.job");
 
 // Action Cache
 const MACTIONS = {};
@@ -28,13 +29,23 @@ model.onKeyUp(({ action, context, device, event, payload }) => {
   MACTIONS[context].update();
 });
 
-model.onDialRotate(({ action, context, device, event, payload }) => {
-  console.log("Your dial code goes here!");
+model.onDidReceiveSettings(({ context, payload }) => {
+  console.log("onDidReceiveSettings", context, payload);
+  MACTIONS[context].didReceiveSettings(payload?.settings);
 });
 
-console.log("model", model);
+job.onWillAppear(({ action, event, context, device, payload }) => {
+  MACTIONS[context] = new JobMonitor(context, payload);
+  $SD.getSettings(context);
+  $SD.getGlobalSettings(context);
+});
 
-model.onDidReceiveSettings(({ context, payload }) => {
+job.onKeyUp(({ action, context, device, event, payload }) => {
+  console.log("Your key code goes here!");
+  MACTIONS[context].update();
+});
+
+job.onDidReceiveSettings(({ context, payload }) => {
   console.log("onDidReceiveSettings", context, payload);
   MACTIONS[context].didReceiveSettings(payload?.settings);
 });
@@ -42,11 +53,124 @@ model.onDidReceiveSettings(({ context, payload }) => {
 const OK_COLOR = "#4bb873";
 const ERROR_COLOR = "#dc3545";
 const MAINTENANCE_COLOR = "#0d6efd";
+const PENDING_COLOR = "#ffc107";
+const QUEUED_COLOR = "#6c757d";
+
+const statusMap = {
+  1: QUEUED_COLOR,
+  2: PENDING_COLOR,
+  3: PENDING_COLOR,
+  10: OK_COLOR,
+  20: ERROR_COLOR,
+  30: PENDING_COLOR,
+};
 
 const filterBy = (str) =>
   items.filter((item) =>
     new RegExp("^" + str.replace(/\*/g, ".*") + "$").test(item)
   );
+
+class JobMonitor {
+  constructor(context, payload) {
+    this.context = context;
+    this.payload = payload;
+    this.size = 48; // default size of the icon is 48
+    this.title = "";
+    this.client = undefined;
+    this.jobId = undefined;
+  }
+
+  didReceiveSettings(settings) {
+    if (!settings) return;
+
+    console.log(settings);
+
+    this.settings = settings;
+
+    this.jobId = settings.jobId;
+    this.client = new DbtCloud(
+      settings.baseUrl,
+      settings.accountId,
+      settings.apiKey
+    );
+
+    this.client.getJob(this.jobId).then((result) => {
+      this.job = result;
+      this.title = result.name;
+      this.update();
+    });
+
+    // $SD.setTitle(this.context, settings.model.replace("_", "_\n"));
+  }
+
+  makeSvg(title, color, value, subtitle) {
+    const scale = 3;
+    const w = this.size * scale;
+
+    const titleSize = 20;
+
+    let textContent = title
+      .match(new RegExp(".{1," + 13 + "}", "g"))
+      .map(
+        (substring, index) =>
+          `<text text-anchor="middle" fill="#ffffff" x="${
+            w / 2
+          }" font-size="${titleSize}px" font-weight="bold" y="${
+            (index + 1) * titleSize
+          }">${substring}</text>`
+      )
+      .reduce((list, item) => {
+        if (list.length < 3) {
+          list.push(item);
+        }
+        return list;
+      }, [])
+      .reduce((content, currentValue) => content + currentValue, "");
+
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="${w}" height="${w}" viewBox="0 0 ${w} ${w}">
+		
+		<rect width="${w}" height="${w}" x="0" y="0" fill="${color}" />
+	${textContent}
+		<text text-anchor="middle" fill="#ffffff" x="${w / 2}" y="${
+      (2 * w) / 3
+    }" font-size="32px" font-weight="bold">${value}</text>
+		<text text-anchor="middle" fill="#ffffff" x="${w / 2}" y="${
+      (5 * w) / 6
+    }" font-size="20px" font-weight="bold">${subtitle}</text>
+
+    </svg>`;
+  }
+
+  update() {
+    this.client.getRuns(this.jobId).then((results) => {
+      if (results.length < 1) {
+        var svg = this.makeSvg(this.title, "ERROR", "NA", "Never Run");
+      } else {
+        let result = results[0];
+        if (result.status >= 10) {
+          let value = result.last_checked_at;
+          var svg = this.makeSvg(
+            this.title,
+            statusMap[result.status],
+            result.last_checked_at.split(" ")[1].split(".")[0],
+            result.last_checked_at.split(" ")[0]
+          );
+        } else {
+          var svg = this.makeSvg(
+            this.title,
+            statusMap[result.status],
+            result.duration,
+            result.status_humanized
+          );
+        }
+      }
+      console.log(svg);
+      var icon = `data:image/svg+xml;base64,${btoa(svg)}`;
+      $SD.setImage(this.context, icon);
+      this.timeout = setTimeout(this.update, 60000);
+    });
+  }
+}
 
 class ModelOverview {
   constructor(context, payload) {
@@ -75,29 +199,33 @@ class ModelOverview {
     this.update();
   }
 
-  makeSvg(title, status, value, subtitle) {
+  makeSvg(title, color, value, subtitle) {
     const scale = 3;
     const w = this.size * scale;
 
     const titleSize = 20;
 
     let textContent = title
-      .split("_")
+      .match(new RegExp(".{1," + 13 + "}", "g"))
       .map(
         (substring, index) =>
           `<text text-anchor="middle" fill="#ffffff" x="${
             w / 2
           }" font-size="${titleSize}px" font-weight="bold" y="${
             (index + 1) * titleSize
-          }">${index >= 1 ? "_" : ""}${substring}</text>`
+          }">${substring}</text>`
       )
+      .reduce((list, item) => {
+        if (list.length < 3) {
+          list.push(item);
+        }
+        return list;
+      }, [])
       .reduce((content, currentValue) => content + currentValue, "");
 
     return `<svg xmlns="http://www.w3.org/2000/svg" width="${w}" height="${w}" viewBox="0 0 ${w} ${w}">
 		
-		<rect width="${w}" height="${w}" x="0" y="0" fill="${
-      status == "OK" ? OK_COLOR : ERROR_COLOR
-    }" />
+		<rect width="${w}" height="${w}" x="0" y="0" fill="${color}" />
 	${textContent}
 		<text text-anchor="middle" fill="#ffffff" x="${w / 2}" y="${
       (2 * w) / 3
@@ -120,16 +248,22 @@ class ModelOverview {
         var icon = `data:image/svg+xml;base64,${btoa(svg)}`;
         $SD.setImage(this.context, icon);
       } else {
+        console.log(filteredResult[0]);
         let value = `${Math.round(filteredResult[0].execution_time)}s`;
         var svg = this.makeSvg(
           this.title,
-          filteredResult[0].status == "success" ? "OK" : "ERROR",
-          value,
-          "Current"
+          filteredResult[0].status == "success"
+            ? OK_COLOR
+            : filteredResult[0].status == "skipped"
+            ? QUEUED_COLOR
+            : ERROR_COLOR,
+          filteredResult[0].status == "skipped" ? "Skipped" : value,
+          filteredResult[0].status == "success" ? "Current" : ""
         );
         console.log(svg);
         var icon = `data:image/svg+xml;base64,${btoa(svg)}`;
         $SD.setImage(this.context, icon);
+        this.timeout = setTimeout(this.update, 60000);
       }
     });
   }
@@ -142,15 +276,39 @@ class DbtCloud {
     this.apiKey = apiKey;
   }
 
-  async getRuns(jobId) {
+  async getJob(jobId) {
     // Find the latest run
 
-    let params = new URLSearchParams({
+    const response = await fetch(
+      `${this.baseUrl}/api/v2/accounts/${this.accountId}/jobs/${jobId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${this.apiKey}`,
+        },
+      }
+    );
+    let content = await response.json().then((result) => {
+      return result;
+    });
+    return content.data;
+  }
+
+  async getRuns(jobId, status) {
+    // Find the latest run
+
+    let options = {
       limit: 5,
       job_definition_id: jobId,
-      order_by: "-finished_at",
-      status: 10,
-    });
+      order_by: "-id",
+    };
+
+    if (!!status) {
+      options.status = status;
+    }
+
+    let params = new URLSearchParams(options);
     const response = await fetch(
       `${this.baseUrl}/api/v2/accounts/${this.accountId}/runs/?${params}`,
       {
